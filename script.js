9f06c9d478c0d8bc970d44fcb6eb6ef455d22370
admamq08 <136003062+admamq08@users.noreply.github.com>
2023-06-09 01:04:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
-// Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyAAtoGfOIAFPMPNeO4-P_ltVOG-TOdSlPo",
-    authDomain: "pmkswebsite.firebaseapp.com",
-    projectId: "pmkswebsite",
-    storageBucket: "pmkswebsite.appspot.comT",
-    messagingSenderId: "958123421961",
-    appId: "1:958123421961:web:6b647b856d3906aeba63d8"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-var db = firebase.firestore();
-
-// Reference to the plan collection
-var plansRef = db.collection('plans');
-
-// Function to add a plan
 function addPlan() {
-    var planName = document.getElementById('planName').value;
-    var date = document.getElementById('date').value;
-    var venue = document.getElementById('venue').value;
-    var extraInfo = document.getElementById('extraInfo').value;
+    // Retrieve input values
+    var planName = document.getElementById("planName").value;
+    var date = document.getElementById("date").value;
+    var venue = document.getElementById("venue").value;
+    var extraInfo = document.getElementById("extraInfo").value;
 
-    // Add the plan to Firestore
-    plansRef.add({
-        planName: planName,
-        date: date,
-        venue: venue,
-        extraInfo: extraInfo
-    })
-    .then(function(docRef) {
-        console.log("Plan added with ID: ", docRef.id);
-        clearForm();
-    })
-    .catch(function(error) {
-        console.error("Error adding plan: ", error);
-    });
+    // Validate input values
+    if (planName === "" || date === "" || venue === "") {
+        alert("Please fill in all the fields.");
+        return;
+    }
+
+    // Create new row and cells
+    var table = document.getElementById("planTable");
+    var row = table.insertRow(-1);
+    var nameCell = row.insertCell(0);
+    var dateCell = row.insertCell(1);
+    var venueCell = row.insertCell(2);
+    var extraInfoCell = row.insertCell(3);
+
+    // Set cell values
+    nameCell.textContent = planName;
+    dateCell.textContent = date;
+    venueCell.textContent = venue;
+    extraInfoCell.textContent = extraInfo;
+
+    // Set label for extra information cell
+    extraInfoCell.setAttribute("data-label", "Extra Information");
+
+    // Clear input fields
+    document.getElementById("planName").value = "";
+    document.getElementById("date").value = "";
+    document.getElementById("venue").value = "";
+    document.getElementById("extraInfo").value = "";
+
+// Function to add a group photo
+function addPhoto(photoUrl) {
+    var photoContainer = document.getElementById("photoContainer");
+    var photoElement = document.createElement("img");
+    photoElement.src = photoUrl;
+    photoContainer.appendChild(photoElement);
 }
-
-// Function to retrieve plans from Firestore
-function retrievePlans() {
-    plansRef.get().then(function(querySnapshot) {
-        var planList = document.getElementById('planList');
-        planList.innerHTML = '';
-
-        querySnapshot.forEach(function(doc) {
-            var plan = doc.data();
-
-            var li = document.createElement('li');
-            li.innerText = 'Plan: ' + plan.planName +
-                ' | Date: ' + plan.date +
-                ' | Venue: ' + plan.venue +
-                ' | Extra Information: ' + plan.extraInfo;
-
-            planList.appendChild(li
+var firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "pmkswebsite",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var db = firebase.firestore();
+  
+}
